2e2a91742033e51aea3a0133e7f176656a9b5744
JohnTWD <92451399+JohnTWD@users.noreply.github.com>
2024-10-10 21:09:19 +0800

    Revert "refactor databaseutils > playlistdatabaseutils"
    This reverts commit b57696ec1b328d27c9aca8a9dbbf163289cad92e.

--- command/commands/CheckPlaylistCommand.py
+++ command/commands/CheckPlaylistCommand.py
@@ -1,6 +1,6 @@
 import discord
 from command.I_Command import I_Command
-from util.databasing.PlaylistDatabaseUtils import updatePlaylistAndGetResponse
+from util.databasing.DatabaseUtils import updatePlaylistAndGetResponse
 
 class CheckPlaylistCommand(I_Command):
 	def __init__(self):
